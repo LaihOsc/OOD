8b3f166034ba18fdbf493ce0c030e6a328ee0cb6
Marco888 <mhulden@yahoo.se>
2024-05-20 21:38:50 +0300

    Update with menubar

--- JavaPlayer/src/view/MusicOrganizerWindow.java
+++ JavaPlayer/src/view/MusicOrganizerWindow.java
@@ -1,5 +1,9 @@
 package view;
 	
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
@@ -8,11 +12,16 @@ import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 import model.Album;
 import model.SoundClip;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.ScrollPane.ScrollBarPolicy;
 import javafx.scene.control.SelectionMode;
@@ -20,9 +29,8 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextInputDialog;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
-
+import javafx.scene.layout.VBox;
 
 
 public class MusicOrganizerWindow extends Application {
@@ -34,7 +42,7 @@ public class MusicOrganizerWindow extends Application {
 	private ButtonPaneHBox buttons;
 	private SoundClipListView soundClipTable;
 	private TextArea messages;
-	
+	private MenuBar menuBar;
 	
 	public static void main(String[] args) {
 		controller = new MusicOrganizerController();
@@ -58,9 +66,41 @@ public class MusicOrganizerWindow extends Application {
 			
 			bord = new BorderPane();
 			
+			// Create menubar
+			MenuItem menuItem1 = new MenuItem("Save As...");
+			menuItem1.setOnAction(e->{
+				FileChooser saveChooser = new FileChooser();
+				saveChooser.setTitle("Save album as...");
+				saveChooser.getExtensionFilters().addAll(
+				         new ExtensionFilter("Album Hierarchy", "*.ahl"),
+				         new ExtensionFilter("Export HTML", "*.html"));
+				File selectedFile = saveChooser.showSaveDialog(primaryStage);
+				if (selectedFile!=null)
+					saveAlbumsTo(selectedFile);
+			});
+			MenuItem menuItem2 = new MenuItem("Load Hierarchy");
+			menuItem2.setOnAction(e->{
+				FileChooser loadChooser = new FileChooser();
+				loadChooser.setTitle("Load album...");
+				loadChooser.getExtensionFilters().addAll(
+				         new ExtensionFilter("Album Hierarchy", "*.ahl"));
+				File selectedFile = loadChooser.showOpenDialog(primaryStage);
+				if (selectedFile!=null)
+					loadAlbumsFrom(selectedFile);
+			});
+			MenuItem menuItem3 = new MenuItem("Exit");
+			menuItem3.setOnAction(e->{
+				Platform.exit();
+				System.exit(0);
+			});
+			Menu menu = new Menu("File");
+			menu.getItems().addAll(menuItem1,menuItem2,menuItem3);
+			menuBar = new MenuBar(menu);
+			
 			// Create buttons in the top of the GUI
 			buttons = new ButtonPaneHBox(controller, this);
-			bord.setTop(buttons);
+			VBox vBox = new VBox(menuBar, buttons);
+			bord.setTop(vBox);
 
 			// Create the tree in the left of the GUI
 			tree = createTreeView();
@@ -88,7 +128,7 @@ public class MusicOrganizerWindow extends Application {
 				}
 				
 			});
-
+			
 			primaryStage.show();
 			
 			
@@ -188,7 +228,22 @@ public class MusicOrganizerWindow extends Application {
 		return soundClipTable.getSelectedClips();
 	}
 	
-	
+	private void saveAlbumsTo(File outFile)
+	{
+		try {
+			FileOutputStream fout = new FileOutputStream(outFile, true);
+			fout.close();
+		} catch (FileNotFoundException e) {
+			displayMessage("Failed to write output file: "+outFile.toString());
+			return;
+		} catch (IOException e) {
+		}
+		displayMessage("Exported hierarchy to: "+outFile.toString());
+	}
+	private void loadAlbumsFrom(File outFile)
+	{
+		displayMessage("Imported hierarchy from: "+outFile.toString());
+	}
 	
 	/**
 	 * *****************************************************************
